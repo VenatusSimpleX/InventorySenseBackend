1185c129916dd5ac7ea57f1dc99e28c66674afa1
VenatusSimpleX <trollingwarfare@yahoo.com>
2019-08-17 02:29:00 +0800

    Implemented original history data GET

--- backend.py
+++ backend.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import jsonify
+
 import pandas
 import random
 
 app = Flask(__name__)
 
 DATA = pandas.read_csv('cleaned_data.csv')
+print(DATA.columns)
 INVENTORY = []
 
 for i in range(3, len(DATA.columns) - 1):
@@ -16,6 +18,24 @@ for i in range(3, len(DATA.columns) - 1):
     'status': 'In stock'
   })
 
+def get_item_name(code):
+  for i in range(len(INVENTORY)):
+    if code == INVENTORY[i]['itemCode']:
+      return INVENTORY[i]['name']
+
+def get_item_original_history(item_name):
+  output = []
+
+  filtered = DATA[DATA[item_name] != 0]
+  
+  for index, row in filtered.iterrows():
+    output.append({
+      'timestamp': row.date + ' ' + row['arrival_ timestamp'],
+      'amount': row[item_name]
+    })
+  
+  return output
+
 @app.route('/')
 def index_not_found():
   return jsonify({
@@ -25,3 +45,15 @@ def index_not_found():
 @app.route('/api/inventory')
 def get_inventory():
   return jsonify(INVENTORY), 200
+
+@app.route('/api/history/<int:item_code>')
+def get_item_history(item_code):
+  item_name = get_item_name(item_code)
+
+  original = get_item_original_history(item_name)
+
+  return jsonify({
+    'original': original,
+    'statistics': [],
+    'machine': []
+  }), 200
